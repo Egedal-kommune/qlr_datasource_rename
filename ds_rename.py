270439a5d23e5e57980cbc317a2b24ed99b962c6
danielarnason <danielarnason85@gmail.com>
2019-02-04 13:11:55 +0100

    kan ny hente qlr filer

--- ds_rename.py
+++ ds_rename.py
@@ -5,7 +5,7 @@ import re
 class QlrReplace:
 
     def __init__(self, path, substitutions):
-        self.path = Path(fr'{path}')
+        self.path = Path(path)
         self.substitutions = substitutions
     
     def replace_substitutions(self, string):
@@ -13,17 +13,19 @@ class QlrReplace:
         regex = re.compile('|'.join(map(re.escape, substrings)))
         return regex.sub(lambda match: self.substitutions[match.group(0)], string)
 
-    def erstat_datasource(self, qlr_fil):
-        qlr_path = self.path.joinpath(qlr_fil)
+    def erstat_datasource(self, qlr_path):
         with open(qlr_path.absolute()) as qlr_f:
             logger.debug(f'Åbner: {qlr_path.name}')
             lines = qlr_f.readlines()
 
         with open(qlr_path.absolute(), 'w') as ny_qlr:
-            logger.debug(f'Overskriver: {qlr_path.name}')
             for line in lines:
                 if '<datasource>' in line:
                     new_line = self.replace_substitutions(line)
                     ny_qlr.write(new_line)
                 else:
                     ny_qlr.write(line)    
+    
+    def get_files(self):
+        qlr_files = self.path.rglob('*.qlr')
+        return qlr_files
